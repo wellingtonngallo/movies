f948749a8a2ba43055224a0d5b6ec0b175f38e08
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-09-18 16:05:05 -0300

    feat: ajusta performance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
 import { useGenre } from "../context/genre";
 
 export function Header() {
-  debugger;
   const { selectedGenre } = useGenre();
   
   return (
     <header>
-      <span className="category">Categoria:<span> </span></span>
+      <span className="category">Categoria:<span>{selectedGenre.title}</span></span>
     </header>
   )
 }
